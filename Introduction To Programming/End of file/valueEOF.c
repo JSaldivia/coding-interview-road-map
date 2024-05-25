52aa7567b09711dd4142e5b1d9c7dbe86d3cb302
Jose <jsaldivi@ucsc.edu>
2024-05-25 11:54:21 -0700

    Testing getchar() function and putchar() function, also printing the value for EOF and explaining why it is -1. Happy programming happy code 👻🧙‍♂️ Vs 🏴‍☠️

---
+++ Introduction To Programming/End of file/valueEOF.c
@@ -0,0 +1,44 @@
+/*
+This program should print the value on the end of file 
+*/
+
+
+/*
+This code block will compare if (getchar() != EOF)
+this will return a 1 or 6 for the variable c
+since booleans (true or false) are returning 1 or 0
+will print the code miltiple times until the 0 is reach
+or EOF (EOF = 0).
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int c;
+    while (c = getchar() != EOF){
+        printf("The value of c is: %d \n", c);
+        putchar(c);
+        printf("The value of c is: %d \n", c);
+
+    }
+    printf("Exit");
+
+    return 0;
+}
+*/
+
+/*snipet to print the value of EOF*/
+
+/*
+EOF evaluates to -1 because all characters are positive numbers
+it was needed to have adistinctive break point (escape character)
+for the function EOF to work properly
+*/
+
+#include <stdio.h>
+
+int main() {
+        printf("The value of EOF is: %d\n", EOF);
+    return 0;
+}
